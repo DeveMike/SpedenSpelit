702274bd9dc4b06e52a58a26b12aab7418b9b423
andyffl <t2koan07@students.oamk.fi>
2024-10-07 19:14:17 +0300

    v2.6 display.h

--- display.h
+++ display.h
@@ -3,7 +3,7 @@
 
 void initializeDisplay(void); // Alustaa 7-segmenttinäytön
 void clearDisplay(); // Tyhjentää näytön ja sammuttaa kaikki segmentit
-// Päivittää 7- kymmenillä, ykkösillä, valinnaisella desimaalipisteellä ja sadoilla.
+// Päivittää 7-segmenttinäytön kymmenillä, ykkösillä, valinnaisella desimaalipisteellä ja sadoilla.
 void updateShiftRegister(int tensDigit, int onesDigit, bool showDots = false, int hundreds = 0);
 
 #endif
