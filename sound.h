0029f6c387ffac9634471b6a640fb497ce77646d
nikohos <niko.hostikka@gmail.com>
2024-10-10 07:28:04 +0300

    Update sound.h

--- sound.h
+++ sound.h
@@ -1,9 +1,9 @@
 #ifndef SOUND_H
 #define SOUND_H
 
-#include <Arduino.h> // Arduinon peruskirjasto
+#include <Arduino.h> // Include core Arduino functionality
 
-void initializeSpeaker(void); // Alustaa kaiutin
-void sound(uint8_t note_index); // Toistaa äänen joka vastaa annettua note_index-arvoa
+void initializeSpeaker(void);       // Initializes the speaker
+void sound(uint8_t note_index);     // Plays a sound effect based on the note index
 
 #endif
