1b3b6d6d08f231bbe88f5d49ef33ec15c8ed7861
nikohos <niko.hostikka@gmail.com>
2024-10-10 07:19:46 +0300

    Update SpedenSpelit.h

--- SpedenSpelit.h
+++ SpedenSpelit.h
@@ -1,18 +1,18 @@
 #ifndef SPEDENSPELIT_H
 #define SPEDENSPELIT_H
-#include <arduino.h> // Arduinon peruskirjasto
-#include <avr/io.h> // I/O rekisterit
-#include <avr/interrupt.h> // Keskeytyksiin liittyvät funktiot ja makrot
-#include <avr/sleep.h> // Mikrokontrollerin lepotilat
-#include <avr/power.h> // Virranhallinnan funktiot
+#include <arduino.h> // Arduino core library
+#include <avr/io.h> // I/O registers
+#include <avr/interrupt.h> // Functions and macros related to interrupts
+#include <avr/sleep.h> // Microcontroller sleep modes
+#include <avr/power.h> // Power management functions
 
-void endGame(); // Päättää pelin ja tallentaa Highscoren
-void initializeGame(); // Alustaa pelin muuttujat
-void checkGame(int pressedButton); // Tarkistaa pelaajan painalluksen oikeellisuuden
-void startTheGame(void); // Käynnistää pelin alusta
-void checkStartCondition(void); // Täyttyvätkö pelin aloitusehdot
-void handleGameLoop(void); // Pelin pääsilmukka. Hallitsee pelin etenemistä
-void initializeTimer(void); // Alustaa Timer1:n ja keskeytykset
-void initializeGameComponents(void); // Alustaa pelin komponentit kuten napit ja LEDit
+void endGame(); // Ends the game and saves the high score
+void initializeGame(); // Initializes game variables
+void checkGame(int pressedButton); // Checks the validity of the player's button press
+void startTheGame(void); // Starts the game from the beginning
+void checkStartCondition(void); // Checks if the conditions to start the game are met
+void handleGameLoop(void); // Main loop of the game. Manages game progression
+void initializeTimer(void); // Initializes Timer1 and interrupts
+void initializeGameComponents(void); // Initializes game components like buttons and LEDs
 
 #endif
